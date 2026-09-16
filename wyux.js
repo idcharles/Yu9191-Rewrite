5bc3aeb90313a8ce409f2ba0a5b56fcceeeb27af
idcharles <30426044+idcharles@users.noreply.github.com>
2026-09-16 10:28:03 +0800

    Update URL in wyux.js for script response

--- wyux.js
+++ wyux.js
@@ -3,7 +3,7 @@
  * 外研u学
 
 [rewrite_local]
-https?:\/\/(www\.unischool\.cn\/api\/(student\/v1\/(course_template\/goods\/query|goods\/album\/detail|course_template\/group\/query|vip\/my\/card\/list|user\/area\/book_detail|goods\/real\/detail)|common\/v1\/account\/user\/student\/info)|h5\.mailejifen\.com\/site\/get-index).* url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/wyux.js
+https?:\/\/(www\.unischool\.cn\/api\/(student\/v1\/(course_template\/goods\/query|goods\/album\/detail|course_template\/group\/query|vip\/my\/card\/list|user\/area\/book_detail|goods\/real\/detail)|common\/v1\/account\/user\/student\/info)|h5\.mailejifen\.com\/site\/get-index).* url script-response-body https://raw.githubusercontent.com/idcharles/Yu9191-Rewrite/refs/heads/main/wyux.js
 
 [mitm]
 hostname = www.unischool.cn, h5.mailejifen.com
